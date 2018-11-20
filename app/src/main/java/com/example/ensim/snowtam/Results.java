4377150753ae7f18417871d4b5ae57ed8353e9cb
Unknown <matthieurode@yahoo.fr>
2018-11-20 09:45:58 +0100

    Page Result Complete  + Airports Model + Correction Manifest

--- app/src/main/java/com/example/ensim/snowtam/Results.java
+++ app/src/main/java/com/example/ensim/snowtam/Results.java
@@ -13,10 +13,12 @@ public class Results extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
+        //Tableau contenant tous les aeroports
         final ArrayList<String> airports = getIntent().getStringArrayListExtra("airports");
+        int numberAirport = airports.size(); //Contient le nombre d'aeroport
+        Log.d("Nombre d'aeroports : ", String.valueOf(numberAirport));
+        Log.d("Nom des aeroports", String.valueOf(airports));
 
 
-
-        Log.d("AIRPORTSSSSSS", String.valueOf(airports));
     }
 }
