ab4e4ac493eb4616a899f6af1af47319f0e3bed0
Unknown <matthieurode@yahoo.fr>
2018-11-21 18:50:52 +0100

    Test Swippe

--- app/src/main/java/com/example/ensim/snowtam/MainAirport.java
+++ app/src/main/java/com/example/ensim/snowtam/MainAirport.java
@@ -6,27 +6,35 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.view.GestureDetectorCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.view.GestureDetector;
 import android.view.MotionEvent;
-import android.view.View;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 
-public class MainAirport extends AppCompatActivity implements OnMapReadyCallback{
+public class MainAirport extends AppCompatActivity implements OnMapReadyCallback {
     private GoogleMap mMap;
     private int index;
     private ArrayList<Airport> listAirport;
+    private GestureDetectorCompat gestureDetectorCompat = null;
+    private TextView textView = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_airport);
+
+        /*GESTURE*/
+        DetectSwipeGestureListener gestureListener = new DetectSwipeGestureListener();
+        gestureListener.setActivity(this);
+        gestureDetectorCompat = new GestureDetectorCompat(this, gestureListener);
+
+
+
+
 
         TextView airportName = findViewById(R.id.mainAirportName);
         TextView longitude = findViewById(R.id.longitude);
@@ -41,6 +49,23 @@ public class MainAirport extends AppCompatActivity implements OnMapReadyCallback
 
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapViewAirport);
         mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        // Pass activity on touch event to the gesture detector.
+        gestureDetectorCompat.onTouchEvent(event);
+        // Return true to tell android OS that event has been consumed, do not pass it to other event listeners.
+        return true;
+    }
+
+    public void displayMessage(String message)
+    {
+        if(textView!=null)
+        {
+            // Display text in the text view.
+            textView.setText(message);
+        }
     }
 
     @Override
@@ -64,47 +89,5 @@ public class MainAirport extends AppCompatActivity implements OnMapReadyCallback
             }
         });
 
-    }
-
-    private final GestureDetector gestureDetector;
-
-    public MainAirport(Context context) {
-        gestureDetector = new GestureDetector(context, new GestureListener());
-    }
-
-    public void onSwipeLeft() {
-    }
-
-    public void onSwipeRight() {
-    }
-
-    public boolean onTouch(View v, MotionEvent event) {
-        return gestureDetector.onTouchEvent(event);
-    }
-
-
-    private final class GestureListener extends GestureDetector.SimpleOnGestureListener {
-
-        private static final int SWIPE_DISTANCE_THRESHOLD = 100;
-        private static final int SWIPE_VELOCITY_THRESHOLD = 100;
-
-        @Override
-        public boolean onDown(MotionEvent e) {
-            return true;
-        }
-
-        @Override
-        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-            float distanceX = e2.getX() - e1.getX();
-            float distanceY = e2.getY() - e1.getY();
-            if (Math.abs(distanceX) > Math.abs(distanceY) && Math.abs(distanceX) > SWIPE_DISTANCE_THRESHOLD && Math.abs(velocityX) > SWIPE_VELOCITY_THRESHOLD) {
-                if (distanceX > 0)
-                    onSwipeRight();
-                else
-                    onSwipeLeft();
-                return true;
-            }
-            return false;
-        }
     }
 }
